bd59bae7fc804accf476f618c72e9db936789a9d
Ian Soares <ian.soaresss@gmail.com>
2022-11-24 09:52:35 -0300

    fix: adding git to install script

--- 4-module/jenkins/setup_jenkins_master.sh
+++ 4-module/jenkins/setup_jenkins_master.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-
 amazon-linux-extras install java-openjdk11 -y
-
 wget -O /etc/yum.repos.d/jenkins.repo  https://pkg.jenkins.io/redhat-stable/jenkins.repo
-
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-
 yum install jenkins -y
-
+yum install git -y
 systemctl enable --now  jenkins
 
