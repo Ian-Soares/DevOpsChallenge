85d4e8661a8983887c6cfd2a575f9ff643828552
Ian Soares <ian.soaresss@gmail.com>
2022-11-24 13:26:27 -0300

    feat: adding kubectl installation

--- 4-module/jenkins/setup_jenkins_master.sh
+++ 4-module/jenkins/setup_jenkins_master.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
 amazon-linux-extras install java-openjdk11 -y
 wget -O /etc/yum.repos.d/jenkins.repo  https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum install jenkins -y
-yum install git -y
+yum install jenkins git docker -y
 systemctl enable --now  jenkins
-
+usermod -a -G docker jenkins
+curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.13/2022-10-31/bin/linux/amd64/kubectl --output-dir /usr/bin/
+chmod +x /usr/bin/kubectl
+systemctl restart docker
