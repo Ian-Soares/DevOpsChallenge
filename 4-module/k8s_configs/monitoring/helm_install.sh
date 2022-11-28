b3f5d04af21423ed70572bcefb5ae67442b03f39
Ian Soares <ian.soaresss@gmail.com>
2022-11-28 18:21:34 -0300

    feat: updating monitoring stack to eks

--- 4-module/k8s_configs/monitoring/helm_install.sh
+++ 4-module/k8s_configs/monitoring/helm_install.sh
@@ -2,8 +2,7 @@
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
-helm install prometheus prometheus-community/prometheus --namespace prometheus --set alertmanager.persistentVolume.storageClass="gp3" --set server.persistentVolume.storageClass="gp3"
-printf 'set admin password: '
-read admin_passwd
-helm install grafana grafana/grafana --namespace grafana --set persistence.storageClassName="gp3" --set persistence.enabled=true --set adminPassword=$admin_passwd --values ./grafana.yaml --set service.type=LoadBalancer
-kubectl port-forward -n prometheus deploy/prometheus-server 8080:9090
+helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring
+
+#kubectl port-forward svc/prometheus-kube-prometheus-prometheus -n monitoring 9090
+#kubectl port-forward svc/prometheus-grafana -n monitoring 8080:80
